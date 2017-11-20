2d0314358e2805779f6db4573674f728447c94c3
PlasticTurd <jamiee.steward@gmail.com>
2017-11-20 18:00:53 +1100

    0.00.1
    implemented retrieval of values from the plaintexts when the button is pressed

--- app/src/main/java/com/example/j/test2/MainActivity.java
+++ app/src/main/java/com/example/j/test2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.j.test2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View view) {
-        
+
+        EditText inputWidthClone = (EditText) findViewById(R.id.inputWidth);
+        EditText inputHeightClone = (EditText) findViewById(R.id.inputHeight);
+        String stringWidth = inputWidthClone.getText().toString();
+        String stringHeight = inputHeightClone.getText().toString();
+        double dubWidth = Double.parseDouble(stringWidth);
+        double dubHeight = Double.parseDouble(stringHeight);
+
     }
 }
